61abb3becb9ba7d8ed4796ebec96d70fbeaaaeda
Piyush <piyushtiwarindls220@gmail.com>
2024-09-13 00:25:15 +0530

    fixed env error

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,9 @@
+import SUPABASE_KEY from "../../env";
+
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://eiqmbegnmyfzojgqabrp.supabase.co";
-// eslint-disable-next-line no-undef
-const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabaseKey = SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
